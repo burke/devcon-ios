2232d1e2d0fce7676d495b5c7b870be5623c7f44
Burke Libbey <burke@burkelibbey.org>
2012-04-19 00:23:34 -0400

    Initial Commit

---
+++ Devcon/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Devcon
+//
+//  Created by Burke Libbey on 12-04-19.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
